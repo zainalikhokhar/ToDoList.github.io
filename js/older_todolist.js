04a1c8c3389b47e1ebfdb01b4fe8e0cf6ccf387d
Zain Ali Khokhar <zainalikhokhar40@gmail.com>
2022-07-03 18:52:12 +0500

    updating design of todolist

--- js/older_todolist.js
+++ js/older_todolist.js
@@ -10,50 +10,34 @@ function populate_table(){
 
 //calling this function will delete all task rows in the todolist table
 function clear_table(){
-
-    var table_heading = document.getElementById("tableHeader")
-    while(table_heading.hasChildNodes()){
-        table_heading.removeChild(table_heading.firstChild)
-    }
     var table_body = document.getElementById("todolist_items") //access the table body
     while(table_body.hasChildNodes()){  //remove all rows in table body
         table_body.removeChild(table_body.firstChild)
     }
-
 }
 
 //this fucntion updates the user's todo liat
 function update_list(){ 
     clear_table() //clear all task rows to prevent rpetition of data
-        header_todo_list = document.getElementById('tableHeader')
-        created_header_row = document.createElement('tr')
 
-    if (JSON.parse(localStorage.getItem(sessionStorage.current_user)).list.length != 0){
-        
-        created_header_row.innerHTML = `<th></th><th >Title</th><th>Description</th><th class="dateColumn">Date</th><th class="deleteColumn"></th>`
-        header_todo_list.appendChild(created_header_row)
-
-
-        user_todo_list = JSON.parse(localStorage.getItem(sessionStorage.current_user)).list // get current_user 's list from localstorage
-        table_todo_list = document.getElementById('todolist_items') //access the the tbody tag of todo list table
-        user_todo_list.forEach( (element,index) =>{ //iterate over each element in the user's list on local storage, and add it as a row in the table
+    user_todo_list = JSON.parse(localStorage.getItem(sessionStorage.current_user)).list // get current_user 's list from localstorage
+    table_todo_list = document.getElementById('todolist_items') //access the the tbody tag of todo list table
+   
+    user_todo_list.forEach( (element,index) =>{ //iterate over each element in the user's list on local storage, and add it as a row in the table
 
         created_row = document.createElement('tr') //create a new row
         created_row.classList.add(`row_${index}`) 
-        created_row.innerHTML = `<td style="padding-left:10px;">${index+1})</td> 
-        <td >${element.title}</td>
-        <td >${element.description}</td>
-        <td style="width:111px">${element.task_date}</td>
-        <td><div   onclick="delete_this(${index})" class="delete_button" ><i class=" text-danger fa-solid fa-trash-can"></i></div></td>`
+        created_row.innerHTML = `<th scope="row" >${index+1}}</th> 
+        <td>${element.title}</td>
+        <td>${element.description}</td>
+        <td style="width:100px">${element.task_date}</td>
+        <td><div   onclick="delete_this(${index})" class="btn delete_button" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+      </svg></div></td>`
 
         table_todo_list.appendChild(created_row) //insert the new row as new child of tbody 
     })
-    }
-    else{
-        created_header_row.innerHTML = `<th class="text-center" ><h3>Your ToDo List is empty</h3></th>`
-        header_todo_list.appendChild(created_header_row)
-    }
-    
 
 }
 
@@ -61,7 +45,7 @@ function update_list(){
 //call populate_table function once the page has loaded
 document.addEventListener("DOMContentLoaded", function(){ 
     add_heading()
-    // populate_table()
+    populate_table()
     update_list()
 })
 
@@ -115,7 +99,7 @@ form_reset.addEventListener("click",function(){
 function delete_this(index_num){
     user_info = JSON.parse(localStorage.getItem(sessionStorage.current_user))
    
-    user_info.list.splice(index_num,1)//delete the objdct at this index number from the users todo list array
+    user_info.list.splice(index_num,1)//delte the objdct at this index number from the users todo list array
     localStorage.setItem(sessionStorage.current_user,JSON.stringify(user_info)) //storing the user_info with updated list on localStorage
     update_list() //update the todo list table
     
